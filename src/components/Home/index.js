0c1296b3ce9bedda2777bf0d5166223f4b7bb485
Jay Jin <jayjinjay@gmail.com>
2019-09-08 16:48:52 +0900

    Filter by uid

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import firebase from "firebase";
+import get from "lodash/fp/get";
+import { savePhraseToFirebase, getPhrases, getPhrasesByUser } from "../../util/firebase-db"
 
-const Landing = () => (
-  <div>
-    <h1>Home</h1>
-  </div>
-);
+const Landing = () => {
+  const [user, initialising, error] = useAuthState(firebase.auth());
+  const [phraseText, setPhraseText] = useState(null);
+  const [phraseList, setPhraseList] = useState([]);
+  console.log({user})
+  // getPhraseList();
+
+  async function getList() {
+    const userId = get("uid", user)
+    // const phrases = await getPhrases();
+    const phrases = await getPhrasesByUser(userId);
+    setPhraseList(phrases);
+  }
+
+  useEffect(() => {
+    getList();
+  }, [])
+
+  console.log({phraseList})
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (phraseText && user) {
+      const userId = get("uid", user)
+      savePhraseToFirebase({userId, phraseText})
+    }
+  }
+  return (
+    <div>
+      <h1>Home {get("email", user)}</h1>
+      <form onSubmit={handleSubmit}>
+        <textarea value={phraseText} onChange={e => setPhraseText(e.target.value)} />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
 
 export default Landing;
--- src/util/firebase-db.js
+++ src/util/firebase-db.js
@@ -1,32 +1,60 @@
 // firebase real-time db
-import firebase from 'firebase';
+import firebase from "firebase";
 
-function saveEndpointToFirebase(endpoint, email) {
-	if (!email) {
-		return Promise.reject({ message: 'login account is not included' });
-	}
-	const id = email.split('@').shift();
-	return firebase
-		.database()
-		.ref(`users/${id}`)
-		.set({ endpoint, time: getCurrentTime() });
+function savePhraseToFirebase({ phraseText, userId }) {
+  return firebase
+    .database()
+    .ref(`phrases`)
+    .push()
+    .set({ phraseText, userId, timestamp: getCurrentTime() });
+  // return firebase
+  // 	.database()
+  // 	.ref(`users/${id}`)
+  // 	.set({ endpoint, time: getCurrentTime() });
 }
 
 function removeDeviceKeyInFirebase() {
-	// Firebase 의 DB data 삭제 API 를 이용하여
-	// Disable 한 기기의 키 값을 제거해보세요.
+  // Firebase 의 DB data 삭제 API 를 이용하여
+  // Disable 한 기기의 키 값을 제거해보세요.
 }
 
 async function getUserList() {
-	const snapshot = await firebase
-		.database()
-		.ref('/users/')
-		.once('value');
-	return snapshot.val();
+  const snapshot = await firebase
+    .database()
+    .ref("/users/")
+    .once("value");
+  return snapshot.val();
+}
+
+async function getPhrases() {
+  const snapshot = await firebase
+    .database()
+    .ref("/phrases/")
+    .once("value");
+  return snapshot.val();
+}
+
+async function getPhrasesByUser(userId) {
+  if (!userId) {
+    return;
+  }
+  const snapshot = await firebase
+    .database()
+    .ref("/phrases/")
+    .orderByChild("userId")
+    .equalTo(userId)
+    .once("value");
+  return snapshot.val();
 }
 
 function getCurrentTime() {
-	return new Date().toLocaleString();
+  return new Date().getTime();
 }
 
-export { saveEndpointToFirebase, removeDeviceKeyInFirebase, getUserList };
+export {
+  savePhraseToFirebase,
+  removeDeviceKeyInFirebase,
+  getUserList,
+  getPhrases,
+  getPhrasesByUser
+};
