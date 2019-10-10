a7bf5605d8174131df903ce1018943593dcbedc3
Jay Jin <jayjinjay@gmail.com>
2019-10-10 18:54:54 +0900

    modify tslint option

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,59 +1,37 @@
-import React, { FunctionComponent, useState, useEffect } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import firebase from 'firebase';
-import moment from 'moment';
-import get from 'lodash/fp/get';
-import map from 'lodash/fp/map';
-import { savePhraseToFirebase, getPhrasesByUser } from '../../util/firebase-db';
+import React, { FunctionComponent, useState, useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import firebase from "firebase";
+import moment from "moment";
+import get from "lodash/fp/get";
+import map from "lodash/fp/map";
+import { getPhrasesByUser } from "../../util/firebase-db";
+import PhraseInput from "./PhraseInput";
 
-const Landing:FunctionComponent<{ initial?: number }> = ({ initial = 0 }) => {
-
+const Landing: FunctionComponent<{}> = () => {
   const [user, initialising, error] = useAuthState(firebase.auth());
-  const [phraseText, setPhraseText] = useState('');
-  const [phraseTrans, setPhraseTrans] = useState('');
   const [phraseList, setPhraseList] = useState([]);
 
   async function getList() {
-    const userId = get('uid', user);
+    const userId = get("uid", user);
     const phrases = await getPhrasesByUser(userId);
     setPhraseList(phrases);
   }
 
   useEffect(() => {
+    // TODO: Check if there's unnessasary call
     getList();
   }, [user]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (phraseText && phraseTrans && user) {
-      const userId = get('uid', user);
-      savePhraseToFirebase({ userId, phraseText, phraseTrans });
-    }
-  };
   return (
     <div>
-      <h1>Home {get('email', user)}</h1>
-      <form onSubmit={handleSubmit}>
-        <textarea
-          value={phraseText}
-          name="phraseText"
-          placeholder="외우고 싶은 문장을 적어주세요"
-          onChange={(e) => setPhraseText(e.target.value)}
-        />
-        <textarea
-          value={phraseTrans}
-          name="phraseTrans"
-          placeholder="문장 뜻을 적어주세요"
-          onChange={(e) => setPhraseTrans(e.target.value)}
-        />
-        <button type="submit">Submit</button>
-      </form>
+      <h1>Home {get("email", user)}</h1>
+      <PhraseInput getList={getList} />
       {map(
         (phrase) => (
           <div>
-            <div>{moment(get('timestamp', phrase)).format('MMM Do YY')}</div>
-            <div>{get('phraseText', phrase)}</div>
-            <div>{get('phraseTrans', phrase)}</div>
+            <div>{moment(get("timestamp", phrase)).format("MMM Do YY")}</div>
+            <div>{get("phraseText", phrase)}</div>
+            <div>{get("phraseTrans", phrase)}</div>
           </div>
         ),
         phraseList,
